01d9586549faa5770dcd5ef747e03544653a24b4
mallorycoppenrath <malcoppenrath@gmail.com>
2016-02-06 11:50:57 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+attr_accessor :name, :breed
+def initialize(name, breed = "Mut")
+  @name = name
+  @breed = breed
+end
+
+# def name=(name)
+#   @name = name
+# end
+
+# def name
+#   @name = name
+# end
+
+# def breed=(breed)
+#   @breed = breed
+# end
+
+# def breed
+#   @breed = breed
+# end
+
+end
